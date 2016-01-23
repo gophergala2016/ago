6ddd5bddef4a488c7a07c614e165aa6ed58d9a94
SeongJae Park <sj38.park@gmail.com>
2016-01-23 14:37:31 +0900

    add copyright and license information
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- ago.go
+++ ago.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2016 SeongJae Park.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License version 3 as published by
+// the Free Software Foundation.
+
 package main
 
 import (
