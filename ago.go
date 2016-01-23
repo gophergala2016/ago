a007a229965f232a852bea1d431662585b441574
SeongJae Park <sj38.park@gmail.com>
2016-01-23 14:28:04 +0900

    ago: add basic structure for command parsing
    Add a basic switch-case structure for command parsing. Also, implements
    basis for error handling and help message. The help message may not be
    so helpful to someone, though ;)
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- ago.go
+++ ago.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+)
+
+const (
+	CMD = "ago"
+	USAGE = "USAGE: " + CMD + " <commands> [argument ...]\n"
+	NOARG_ERRMSG = USAGE + "\nFor detail, try " + CMD + " help\n"
+	HELP_MSG = "Use the source ;)\n"
+)
+
+var (
+	agol = log.New(os.Stderr, "[ago] ", 0)
+)
 
 func main() {
-	fmt.Println("this is ago")
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("No argument.\n")
+		fmt.Printf(NOARG_ERRMSG)
+		os.Exit(1)
+	}
+	cmd := args[1]
+	args = args[2:]
+	switch cmd {
+	case "help":
+		fmt.Printf(HELP_MSG)
+	default:
+		agol.Printf("wrong commanad")
+		os.Exit(1)
+	}
 }
